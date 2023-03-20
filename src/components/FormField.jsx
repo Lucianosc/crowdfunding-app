8323cf4559038fdbb853ba4290cc32f26feb22cf
Lucianosc <scaminaci.luciano@gmail.com>
2023-03-20 15:08:27 -0300

    Added CreateCampaign page and FormField component

---
+++ src/components/FormField.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function FormField({
+  labelName,
+  placeholder,
+  inputType,
+  value,
+  handleChange,
+  isTextArea,
+}) {
+  return (
+    <label className="flex flex-col flex-1 w-full ">
+      {labelName && (
+        <span className="font-epilogue font-medium text-[14px] leading-[22px] text-[#808191] mb-[10px]">
+          {labelName}
+        </span>
+      )}
+      {isTextArea ? (
+        <textarea
+          required
+          value={value}
+          placeholder={placeholder}
+          onChange={handleChange}
+          rows="10"
+          className="py-[15px] sm:px-[25px] sm:min-w-[300px] px-[15px] outline-none border-[1px]
+         border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placegolder:text-[#4b5264] 
+         rounded-[10px]"
+        ></textarea>
+      ) : (
+        <input
+          required
+          value={value}
+          placeholder={placeholder}
+          type={inputType}
+          onChange={handleChange}
+          step="0.1"
+          className="py-[15px] sm:px-[25px] sm:min-w-[300px] px-[15px] outline-none border-[1px]
+           border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placegolder:text-[#4b5264] 
+           rounded-[10px]"
+        ></input>
+      )}
+    </label>
+  )
+}
