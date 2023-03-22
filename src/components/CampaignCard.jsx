f17cb04a34f68cdb0a551af7379e330c816e21ee
Lucianosc <scaminaci.luciano@gmail.com>
2023-03-22 15:39:48 -0300

    Changes in Home page, DisplayCampaign and CampaignCard components

---
+++ src/components/CampaignCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CampaignCard() {
+  return (
+    <div className="">
+      <p>CampaignCard</p>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useStateContext } from '../context'
+import { DisplayCampaigns } from '../components'
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [campaigns, setCampaigns] = useState([])
+
+  const { address, contract, getCampaigns } = useStateContext()
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true)
+    const data = await getCampaigns()
+    setCampaigns(data)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    if (contract) fetchCampaigns()
+  }, [address, contract])
+
+  console.log(campaigns)
+
   return (
-    <div>Home</div>
+    <DisplayCampaigns
+      title="All campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
   )
 }
