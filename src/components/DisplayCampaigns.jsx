9ef95ac26fb14535738f6d7431bb49bd9333a464
Lucianosc <scaminaci.luciano@gmail.com>
2023-03-29 11:09:31 -0300

    Structure changes on DisplayCampaigns, Home and Profile

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
+import { useStateContext } from '../context'
 import { useNavigate } from 'react-router-dom'
 import { loader } from '../assets'
 import { CampaignCard } from '../components'
 
-export default function DisplayCampaigns({ title, isLoading, campaigns }) {
+export default function DisplayCampaigns({ title, isFilteredByOwner = false }) {
   const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [campaigns, setCampaigns] = useState([])
+
+  const { address, contract, getCampaigns } = useStateContext()
+
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      const data = await getCampaigns(isFilteredByOwner)
+      setCampaigns(data)
+      setIsLoading(false)
+    }
+    if (contract) {
+      fetchCampaigns()
+    }
+  }, [address, contract])
 
   const handleNavigate = (campaign) => {
     navigate(`/campaign-details/${campaign.title}`, { state: campaign })
@@ -16,28 +33,27 @@ export default function DisplayCampaigns({ title, isLoading, campaigns }) {
         {title} ({campaigns.length})
       </h1>
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
-        {isLoading && (
+        {isLoading ? (
           <img
             src={loader}
             alt="loader"
             className="w-[100px] h-[100px] object-contain"
           ></img>
-        )}
-        {!isLoading && campaigns.length === 0 && (
-          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
-            You have not created any campaigns yet
-          </p>
-        )}
-
-        {!isLoading &&
-          campaigns.length > 0 &&
+        ) : campaigns.length > 0 ? (
           campaigns.map((campaign) => (
             <CampaignCard
               key={campaign.id}
               {...campaign}
               handleClick={() => handleNavigate(campaign)}
             />
-          ))}
+          ))
+        ) : (
+          campaigns.length === 0 && (
+            <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+              There are no campaigns yet
+            </p>
+          )
+        )}
       </div>
     </div>
   )
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { useStateContext } from '../context'
+import React from 'react'
+
 import { DisplayCampaigns } from '../components'
 
 export default function Profile() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [campaigns, setCampaigns] = useState([])
-
-  const { address, contract, getCampaigns } = useStateContext()
-
-  const fetchCampaigns = async () => {
-    setIsLoading(true)
-    const data = await getCampaigns(true)
-    setCampaigns(data)
-    setIsLoading(false)
-  }
-
-  useEffect(() => {
-    if (contract) fetchCampaigns()
-  }, [address, contract])
-
-  console.log(campaigns)
-
-  return (
-    <DisplayCampaigns
-      title="All campaigns"
-      isLoading={isLoading}
-      campaigns={campaigns}
-    />
-  )
+  return <DisplayCampaigns title="My campaigns" isFilteredByOwner={true} />
 }
