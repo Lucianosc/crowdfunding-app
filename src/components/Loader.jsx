7f3726c30b5ea616794d506bf9b3246fa9b52527
Lucianosc <scaminaci.luciano@gmail.com>
2023-04-14 15:14:05 -0300

    Changes on Loder component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { loader } from '../assets'
+import { useThemeContext } from '../context/ThemeContext'
 
 export default function Loader() {
+  const { isDarkTheme } = useThemeContext()
   return (
     <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
       <img
@@ -9,7 +11,11 @@ export default function Loader() {
         alt="loader"
         className="w-[100px] h-[100px] object-contain"
       />
-      <p className="mt-[20px] font-bold text-[20px] text-center">
+      <p
+        className={`mt-[20px] font-bold text-[20px] text-center ${
+          isDarkTheme ? 'dark' : 'light'
+        } text-[var(--color-secondary)] leading-7`}
+      >
         Transaction in progress
         <br />
         Please wait...
