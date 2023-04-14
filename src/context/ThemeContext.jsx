2be5b3bd24d0552fea0d87c26d53bfc1d9524ce5
Lucianosc <scaminaci.luciano@gmail.com>
2023-04-14 15:58:32 -0300

    Set Color themes on LocalStorage

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,12 +5,22 @@ const ThemeContext = createContext()
 export const useThemeContext = () => useContext(ThemeContext)
 
 export const ThemeContextProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(true)
-
-  function toggleTheme() {
-    setIsDarkTheme(!isDarkTheme)
+  const getInitialTheme = () => {
+    if (localStorage.getItem('theme') === 'light') return false
+    else return true
   }
 
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialTheme)
+
+  function toggleTheme() {
+    if (localStorage.getItem('theme') !== 'light') {
+      localStorage.setItem('theme', 'light')
+      setIsDarkTheme(false)
+    } else {
+      setIsDarkTheme(true)
+      localStorage.setItem('theme', 'dark')
+    }
+  }
   return (
     <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
       {children}
