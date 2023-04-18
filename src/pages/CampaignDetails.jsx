22d897435756699c9c555b1d9aea538b56cb2fe4
Lucianosc <scaminaci.luciano@gmail.com>
2023-04-17 22:18:22 -0300

    Mayor changes in CampaignDetails page

--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -8,7 +8,9 @@ import { calculateBarPercentage, daysLeft } from '../utils'
 import { thirdweb } from '../assets'
 
 export default function CampaignDetails() {
-  const { state } = useLocation()
+  const {
+    state: { id, target, amountCollected, image, title , owner, description, deadline},
+  } = useLocation()
   const navigate = useNavigate()
   const { isDarkTheme } = useThemeContext()
 
@@ -21,21 +23,25 @@ export default function CampaignDetails() {
   const [isLoading, setIsLoading] = useState(false)
   const [amount, setAmount] = useState(0)
   const [donators, setDonators] = useState([])
-  const remainingDays = daysLeft(state?.deadline)
+  const remainingDays = daysLeft(deadline)
   const [imageError, setImageError] = useState(false)
+  const barPercentage = calculateBarPercentage(
+    target,
+    amountCollected,
+  )
 
   const handleImageError = () => {
     setImageError(true)
   }
 
   const fetchDonators = async () => {
-    const donators = await getDonationsByCampaignId(state.id)
+    const donators = await getDonationsByCampaignId(id)
     setDonators(donators)
   }
 
   const handleDonate = async () => {
     setIsLoading(true)
-    if (amount > 0) await fundCampaign(state.id, amount)
+    if (amount > 0) await fundCampaign(id, amount)
     else console.log('type any amount')
     navigate('/')
     setIsLoading(false)
@@ -48,7 +54,7 @@ export default function CampaignDetails() {
   return (
     <div>
       {isLoading && <Loader />}
-      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+      <div className="w-full flex md:flex-row flex-col mt-4 gap-[30px]">
         <div className="flex-1 flex-col">
           {imageError ? (
             <div className="w-full h-[410px] rounded-[15px] flex justify-center items-center">
@@ -62,41 +68,56 @@ export default function CampaignDetails() {
             </div>
           ) : (
             <img
-              src={state.image}
+              src={image}
               alt="campaign"
               className="w-full h-[410px] object-cover rounded-xl"
               onError={handleImageError}
             />
           )}
-          <div className="relative w-full h-[5px] bg-[var(--color-background2)] mt-2 rounded-full">
+          <div className="relative w-full h-[8px] bg-[var(--color-background2)] mt-2 rounded-full">
             <div
               className="w-[30px] bg-[var(--color-primary)] h-full rounded-full"
               style={{
-                width: `${calculateBarPercentage(
-                  state.target,
-                  state.amountCollected,
-                )}%`,
+                width: `${barPercentage}%`,
                 maxWidth: '100%',
               }}
             ></div>
+          </div>
+          <div className="mt-2">
+            <p
+              className={`font-normal text-[16px] leading-[18px] text-justify ${
+                isDarkTheme ? 'dark' : 'light'
+              } text-[var(--color-text)]`}
+            >
+              {barPercentage}% backed
+            </p>
           </div>
         </div>
         <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
           <CountBox title="Days left" value={remainingDays} />
           <CountBox
-            title={`Raised of ${state.target}`}
-            value={state.amountCollected}
+            title={`Raised of ${target}`}
+            value={amountCollected}
           />
           <CountBox title="Total Backers" value={donators.length} />
         </div>
       </div>
-      <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
+      <div className="mt-12 flex lg:flex-row flex-col gap-5">
         <div className="flex-[2] flex flex-col gap-[40px]">
           <div>
             <h4
-              className={`text-[18px] ${
+              className={`text-[32px] ${
                 isDarkTheme ? 'dark' : 'light'
-              } text-[var(--color-text)] font-semibold uppercase`}
+              } text-[var(--color-text)] font-semibold `}
+            >
+              {title}
+            </h4>
+          </div>
+          <div>
+            <h4
+              className={`text-[20px] ${
+                isDarkTheme ? 'dark' : 'light'
+              } text-[var(--color-text)] font-semibold `}
             >
               Creator
             </h4>
@@ -118,43 +139,39 @@ export default function CampaignDetails() {
                     isDarkTheme ? 'dark' : 'light'
                   } text-[var(--color-text2)] break-all`}
                 >
-                  {state.owner}
+                  {owner}
                 </h4>
-                <p
+                {/* <p
                   className={`mt-[4px] font-normal ${
                     isDarkTheme ? 'dark' : 'light'
                   } text-[var(--color-text2)]`}
                 >
                   10 campaigns
-                </p>
+                </p> */}
               </div>
             </div>
           </div>
 
           <div>
             <h4
-              className={`text-[18px] ${
+              className={`text-[20px] ${
                 isDarkTheme ? 'dark' : 'light'
-              } text-[var(--color-text)] font-semibold uppercase`}
+              } text-[var(--color-text)] font-semibold `}
             >
               Story
             </h4>
             <div className="mt-[20px]">
-              <p
-                className=" font-normal text-[16px] leading-[26px] text-justify ${
-              isDarkTheme ? 'dark' : 'light'
-            } text-[var(--color-text2)]"
-              >
-                {state.description}
+              <p className=" font-normal text-[16px] leading-[26px] text-justify text-[var(--color-text2)]">
+                {description}
               </p>
             </div>
           </div>
 
           <div>
             <h4
-              className={`text-[18px] ${
+              className={`text-[20px] ${
                 isDarkTheme ? 'dark' : 'light'
-              } text-[var(--color-text)] font-semibold uppercase`}
+              } text-[var(--color-text)] font-semibold `}
             >
               Donators
             </h4>
@@ -173,7 +190,7 @@ export default function CampaignDetails() {
                       {index + 1}. {item.donator}
                     </p>
                     <p
-                      className={`text-[16px] ${
+                      className={`text-[16px] min-w-[68px] ${
                         isDarkTheme ? 'dark' : 'light'
                       } text-[var(--color-text2)] leading-[26px] break-all`}
                     >
@@ -194,14 +211,7 @@ export default function CampaignDetails() {
           </div>
 
           <div className="flex-1 justify-center">
-            <h4
-              className={`text-[18px] ${
-                isDarkTheme ? 'dark' : 'light'
-              } text-[var(--color-text)] font-semibold uppercase`}
-            >
-              Fund
-            </h4>
-            <div className="mt-[20px] flex flex-col p-4 bg-[var(--color-background2)] rounded-[10px]">
+            <div className="mt-[20px] flex flex-col p-4 bg-[var(--color-background2)] rounded-[10px] max-w-[720px] mx-auto">
               <p
                 className={`font-medium text-[20px] leading-[30px] text-center ${
                   isDarkTheme ? 'dark' : 'light'
@@ -209,18 +219,8 @@ export default function CampaignDetails() {
               >
                 Fund the campaign
               </p>
-              <div className="mt-[30px] ">
-                <input
-                  type="number"
-                  placeholder="ETH 0.1"
-                  step="0.01"
-                  className={`w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] 
-                  border-[var(--color-text2)] bg-transparent text-[18px] leading-[30px] placeholder:text-[var(--color-text2)] text-[var(--color-text)]
-                  rounded-[10px]`}
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                />
-                <div className="my-[20px] p-4 bg-[var(--color-background)] rounded-[10px]">
+              <div className="mt-[30px] flex flex-col gap-[20px]">
+                <div className=" p-4 bg-[var(--color-background)] rounded-[10px]">
                   <h4
                     className={`font-semibold ${
                       isDarkTheme ? 'dark' : 'light'
@@ -237,6 +237,17 @@ export default function CampaignDetails() {
                     you.
                   </p>
                 </div>
+                <input
+                  type="number"
+                  placeholder="ETH 0.1"
+                  step="0.01"
+                  className={`w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] 
+                  border-[var(--color-text2)] bg-transparent text-[20px] leading-[30px] placeholder:text-[var(--color-text2)] text-[var(--color-text)]
+                  rounded-[10px]`}
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                />
+
                 <div className="flex justify-center">
                   <CustomButton
                     btnType="button"
