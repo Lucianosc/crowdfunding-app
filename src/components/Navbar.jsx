d2a3594cd2f50fefd8c2e89cd1351e9af3c8d2ea
Lucianosc <scaminaci.luciano@gmail.com>
2023-05-09 23:26:55 -0300

    Refactor of Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Link, useNavigate, useLocation } from 'react-router-dom'
-import { CustomButton } from './'
-import { logo, menu, search, thirdweb, sun } from '../assets'
-import { navlinks } from '../constants'
-import { useStateContext } from '../context/StateContext'
-import { useThemeContext } from '../context/ThemeContext'
-import { NavItem } from '../components'
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { CustomButton } from "./";
+import { logo, menu, search, thirdweb, sun } from "../assets";
+import { navlinks } from "../constants";
+import { useStateContext } from "../context/StateContext";
+import { useThemeContext } from "../context/ThemeContext";
+import { NavItem } from "../components";
 
 export default function Navbar({ handleCallback }) {
-  const navigate = useNavigate()
-  const [toggleDrawer, setToggleDrawer] = useState(false)
-  const drawerRef = useRef(null)
-  const { isDarkTheme, toggleTheme } = useThemeContext()
-  const location = useLocation()
+  const navigate = useNavigate();
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const drawerRef = useRef(null);
+  const { isDarkTheme, toggleTheme } = useThemeContext();
+  const location = useLocation();
 
-  const { address, connect } = useStateContext()
-
+  const { isWalletConnected, connectWallet } = useStateContext();
   useEffect(() => {
-    document.addEventListener('click', handleClickOutsideDrawer, true)
+    document.addEventListener("click", handleClickOutsideDrawer, true);
 
     return () => {
-      document.removeEventListener('click', handleClickOutsideDrawer, true)
-    }
-  }, [toggleDrawer])
+      document.removeEventListener("click", handleClickOutsideDrawer, true);
+    };
+  }, [toggleDrawer]);
 
   const handleClickOutsideDrawer = (e) => {
     if (toggleDrawer && !drawerRef.current.contains(e.target)) {
-      setToggleDrawer(false)
+      setToggleDrawer(false);
     }
-  }
+  };
 
   const debounce = (func, delay) => {
     let timeoutId;
@@ -38,35 +37,35 @@ export default function Navbar({ handleCallback }) {
         func(...args);
       }, delay);
     };
-  }  
+  };
 
   const handleSearch = (e) => {
-    const processChange = debounce(() => handleCallback(e.target.value), 600)
-    processChange()
-  }
+    const processChange = debounce(() => handleCallback(e.target.value), 600);
+    processChange();
+  };
 
   return (
     <div className="flex sm:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div
         className={`lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] ${
-          location.pathname === '/' || location.pathname === '/profile'
-            ? ''
-            : ' sm:invisible hidden sm:block'
+          location.pathname === "/" || location.pathname === "/profile"
+            ? ""
+            : " sm:invisible hidden sm:block"
         } ${
-          isDarkTheme ? 'dark' : 'light'
+          isDarkTheme ? "dark" : "light"
         } bg-[var(--color-background2)] rounded-[100px]`}
       >
         <input
           type="text"
           placeholder="Search for campaigns"
           className={`flex w-full foont-roboto text-[14px] ${
-            isDarkTheme ? 'dark' : 'light'
+            isDarkTheme ? "dark" : "light"
           } placeholder:text-[var(--color-text2)] text-[var(--color-text)] bg-transparent outline-none pr-2`}
           onChange={handleSearch}
         />
         <div
           className={`flex justify-center items-center cursor-pointer w-[72px] h-full rounded-[20px] ${
-            isDarkTheme ? 'dark' : 'light'
+            isDarkTheme ? "dark" : "light"
           } bg-[var(--color-primary)]`}
         >
           <img
@@ -80,17 +79,17 @@ export default function Navbar({ handleCallback }) {
       <div className="sm:flex flex-row hidden justify-end gap-4 ">
         <CustomButton
           btnType="button"
-          title={address ? 'Create campaign' : 'Connect wallet'}
+          title={isWalletConnected ? "Create campaign" : "Connect wallet"}
           styles="bg-[var(--color-primary)] text-[var(--color-secondary)]"
           handleClick={() => {
-            if (address) navigate('create-campaign')
-            else connect()
+            if (isWalletConnected) navigate("create-campaign");
+            else connectWallet();
           }}
         />
         <Link to="/profile">
           <div
             className={`flex justify-center items-center w-[52px] h-[52px] rounded-[100px] ${
-              isDarkTheme ? 'dark' : 'light'
+              isDarkTheme ? "dark" : "light"
             } bg-[var(--color-background2)] cursor-pointer`}
           >
             <img
@@ -105,7 +104,7 @@ export default function Navbar({ handleCallback }) {
       <div className="flex justify-between items-center relative sm:hidden">
         <div
           className={`flex justify-center items-center w-[40px] h-[40px] rounded-[10px] ${
-            isDarkTheme ? 'dark' : 'light'
+            isDarkTheme ? "dark" : "light"
           } bg-[var(--color-background2)] cursor-pointer`}
         >
           <img
@@ -122,7 +121,7 @@ export default function Navbar({ handleCallback }) {
         />
         <div
           className={`fixed top-[0px] right-0 left-0 z-10 shadow-secondary  ${
-            toggleDrawer ? 'translate-y-0' : '-translate-y-[100vh] '
+            toggleDrawer ? "translate-y-0" : "-translate-y-[100vh] "
           } transition-all duration-700`}
           ref={drawerRef}
         >
@@ -133,13 +132,13 @@ export default function Navbar({ handleCallback }) {
                   key={link.name}
                   className={`flex p-4 ${
                     location.pathname === link.link &&
-                    'bg-[var(--color-background)] '
+                    "bg-[var(--color-background)] "
                   } 
-                 ${link.disabled && 'opacity-30'}`}
+                 ${link.disabled && "opacity-30"}`}
                   onClick={() => {
                     if (!link.disabled) {
-                      setToggleDrawer(false)
-                      navigate(link.link)
+                      setToggleDrawer(false);
+                      navigate(link.link);
                     }
                   }}
                 >
@@ -148,15 +147,15 @@ export default function Navbar({ handleCallback }) {
                     alt={link.name}
                     className={` w-[24px] h-[24px] object-contain ${
                       location.pathname === link.link
-                        ? 'grayscale-0'
-                        : 'grayscale'
+                        ? "grayscale-0"
+                        : "grayscale"
                     }`}
                   />
                   <p
                     className={`ml-[20px]  font-semibold text-[14px] ${
                       location.pathname === link.link
-                        ? 'text-[var(--color-text)]'
-                        : 'text-[#808191]'
+                        ? "text-[var(--color-text)]"
+                        : "text-[#808191]"
                     }`}
                   >
                     {link.name}
@@ -167,17 +166,17 @@ export default function Navbar({ handleCallback }) {
             <div className="flex mx-4 justify-between">
               <CustomButton
                 btnType="button"
-                title={address ? 'Create campaign' : 'Connect wallet'}
+                title={isWalletConnected ? "Create campaign" : "Connect wallet"}
                 styles="bg-[var(--color-primary)] text-[var(--color-secondary)]"
                 handleClick={() => {
-                  setToggleDrawer(false)
-                  if (address) navigate('create-campaign')
-                  else connect()
+                  setToggleDrawer(false);
+                  if (isWalletConnected) navigate("create-campaign");
+                  else connectWallet();
                 }}
               />
               <NavItem
                 styles={` ${
-                  isDarkTheme ? 'dark' : 'light'
+                  isDarkTheme ? "dark" : "light"
                 } bg-[var(--color-background)] shadow-secondary`}
                 imgUrl={sun}
                 handleClick={() => toggleTheme()}
@@ -187,10 +186,10 @@ export default function Navbar({ handleCallback }) {
         </div>
         <div
           className={`fixed top-0 left-0 bg-[var(--color-text)] opacity-40 h-[1000px] w-full ${
-            toggleDrawer ? 'z-5' : 'z-[-1]'
+            toggleDrawer ? "z-5" : "z-[-1]"
           } transition-all duration-200`}
         ></div>
       </div>
     </div>
-  )
+  );
 }
